e00d57615a5eca406b9105379055a3e73e0829ab
Ceciany <120055945+Ceciany@users.noreply.github.com>
2023-09-26 18:48:38 +0000

    ok

---
+++ main.ts
@@ -0,0 +1,18 @@
+let numero: number = 20;
+let texto: string = "Otávio";
+let nulo: null;
+let boleano: boolean = false;
+let ingredientes = ["açucar", "sal", "limao"];
+let objeto = {nome:"ceciany",idade:10,diploma:true};
+
+
+
+
+
+
+
+
+console.log(objeto);
+
+
+
