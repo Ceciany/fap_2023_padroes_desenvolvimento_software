16ae171441dcf53b89f69ba1b7d82466d5619b4e
Ceciany <120055945+Ceciany@users.noreply.github.com>
2023-10-03 18:24:12 +0000

    ok

--- main.ts
+++ main.ts
@@ -1,7 +1,34 @@
-const 
+// // sem parâmetro e retorno
+// function fun01(): void {
+//     console.log(`Esta é a minha primeira função typescripyt.`);
+    
+// } 
+
+// fun01()
+
+// // com parâmetro e sem retorno
+// function fun02(nome: string): void {
+//     console.log(`Boa tarde, ${nome}.`);
+    
+// } 
+
+// fun02("Claudio")
+
+// sem parâmetro e com retorno
+// function fun03(): number {
+//     return 10;
+    
+// } 
+
+// let valor = fun03();
+// console.log(valor);
 
 
+// com parâmetro e com retorno
+function fun04(n1: number, n2:number): number {
+    return (n1+n2);
+    
+} 
 
-
-
-
+// let soma = fun04(5,7);
+console.log(fun04(5,7));
